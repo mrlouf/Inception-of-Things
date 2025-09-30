daa217ccfd593cc75c739a7df2b6b695d03a2010
nponchon <nponchon@Debian13.0.0.nponchon42>
2025-09-30 17:38:54 +0200

    Argo CD is up and running the app from the dedicated public repo on GitHub. Needs to track the tags to sync accordingly.

---
+++ p3/setup-p3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Install Docker first if not present:
+if ! systemctl is-active --quiet docker; then
+    echo "Installing Docker..."
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    
+    # Actually install Docker
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    
+    # Start and enable Docker service
+    sudo systemctl start docker
+    sudo systemctl enable docker
+else
+    echo "Docker already installed and running"
+fi
+
+# Install k3d
+echo "Starting k3d..."
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+k3d cluster create mycluster --agents 2
+
+# Install kubectl
+echo "Installing kubectl..."
+sudo apt-get install -y kubectl
+kubectl get nodes -o wide
+
+# Install ArgoCD CLI
+echo "Installing ArgoCD CLI..."
+kubectl create namespace argocd -dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for Argo CD admin secret..."
+until kubectl -n argocd get secret argocd-initial-admin-secret &> /dev/null; do
+  sleep 2
+done
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+
+
+VERSION=$(curl -s https://api.github.com/repos/argoproj/argo-cd/releases/latest | grep tag_name | cut -d '"' -f 4)
+curl -sSL -o argocd "https://github.com/argoproj/argo-cd/releases/download/${VERSION}/argocd-linux-amd64"
+chmod +x argocd
+sudo mv argocd /usr/local/bin/
+
+# Install wil42's image from Docker Hub
+echo "Installing wil42's image from Docker Hub..."
+kubectl create namespace dev --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -n dev -f https://raw.githubusercontent.com/mrlouf/nponchon-IoT/main/deployment.yaml
+kubectl apply -f argocd-myapp.yaml
